0293e6fbc0a82cc89c8dce00ac1fc948e7f611f5
Ferrix Hovi <ferrix@ferrix.fi>
2013-02-02 17:14:53 +0200

    Fix fixed date.today problem
    Instead of a function pointer, sent value of today. This results that
    all dates will be the same until the app is restarted. Wrong.

--- fatme/models.py
+++ fatme/models.py
@@ -2,7 +2,7 @@ from couchdbkit.ext.django.schema import *
 from datetime import date
 
 class Weight(Document):
-    date = DateProperty(default=date.today(), required=True)
+    date = DateProperty(default=date.today, required=True)
     weight = FloatProperty(required=True)
 
     def save(self, *args, **kwargs):
